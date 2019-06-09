108b830518b8bd4302bd3f3b8cc277cdd2a26e74
Leonardo Sotelo <30325990+jose-sotelo@users.noreply.github.com>
2019-06-08 21:01:53 -0500

    Add files via upload
    se crea la carpeta public, con las subcarpetas CSS, IMG, JS; asi mismo se agregan los archivos index.php, y compositor.json como descriptor y depencias del proyecto

---
+++ public/js/application.js
@@ -0,0 +1,39 @@
+$(document).ready(() => {
+    if (getCookie('contador') == ''){
+      setCookie('contador', 0, 365);
+    }
+
+    incrementar();
+
+    $('#contador').text('Ud. es el visitante # ' + getCookie('contador'));
+});
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function incrementar(){
+  var valorActual = getCookie('contador');
+  valorActual = parseInt(valorActual) + 1;
+
+  setCookie('contador', valorActual, 365);
+}
